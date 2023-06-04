4a3197ccc2bf85a48ea4227b18da12f4b31be2d3
noaether <67566122+noaether@users.noreply.github.com>
2023-06-04 13:25:04 -0400

    day 4 part 2

--- 2022/day-04.js
+++ 2022/day-04.js
@@ -22,5 +22,25 @@ function countFullyContainedPairs(assignmentPairs) {
   return count;
 }
 
+function countOverlappingPairs(assignmentPairs) {
+  let count = 0;
+
+  for (const pair of assignmentPairs) {
+    const [range1, range2] = pair.split(",");
+
+    const [start1, end1] = range1.split("-").map(Number);
+    const [start2, end2] = range2.split("-").map(Number);
+
+    if (start1 <= end2 && start2 <= end1) {
+      count++;
+    }
+  }
+
+  return count;
+}
+
 const fullyContainedPairs = countFullyContainedPairs(assignmentPairs);
 console.log("Number of fully contained pairs:", fullyContainedPairs);
+
+const overlappingPairs = countOverlappingPairs(assignmentPairs);
+console.log("Number of overlapping assignment pairs:", overlappingPairs);
